ac0e6f62d5dacd71f2e0e8102c105e68fe5ea342
risonakamo <hmykanae@gmail.com>
2020-10-13 21:03:17 -0400

    add empty page template

---
+++ emptypage/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function IndexMain():JSX.Element
+{
+  return <>
+
+  </>;
+}
+
+function main()
+{
+  ReactDOM.render(<IndexMain/>,document.querySelector(".main"));
+}
+
+window.onload=main;
