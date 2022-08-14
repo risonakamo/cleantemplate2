c835bf41e455e7e4951acbb162a0e1b2c773650a
risonakamo <hmykanae@gmail.com>
2022-08-14 16:11:10 -0400

    update vite config

--- webpacks/vite.config-react.ts
+++ webpacks/vite.config-react.ts
@@ -2,6 +2,8 @@ import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 
+declare const __dirname:string;
+
 export default defineConfig({
     plugins:[
         react(),
@@ -9,10 +11,22 @@ export default defineConfig({
             typescript:true
         })
     ],
+
+    resolve:{
+        alias:{
+            components:`${__dirname}/web/components`,
+            lib:`${__dirname}/web/lib`,
+            css:`${__dirname}/web/css`,
+            apis:`${__dirname}/web/apis`,
+            hooks:`${__dirname}/web/hooks`
+        }
+    },
+
     server:{
         port:4000,
         hmr:false
     },
+
     build:{
         outDir:"build"
     }
